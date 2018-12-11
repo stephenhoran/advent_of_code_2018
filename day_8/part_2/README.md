e53de8306b6a79354cf2523b8086f91bfab53e7b
atssteve <steve.horan@theatsgroup.com>
2018-12-10 19:13:32 -0500

    Adding Day 8 Part 2.

--- day_8/part_2/README.md
+++ day_8/part_2/README.md
@@ -1,45 +1,25 @@
-## Day 7 - Part 2
+## Day 8 - Part 2
 
-As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple steps are available, workers should still begin them in alphabetical order.
+--- Part Two ---
+The second check is slightly more complicated: you need to find the value of the root node (A in the example above).
 
-Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+The value of a node depends on whether it has child nodes.
 
-To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the same instructions as above, this is how each second would be spent:
+If a node has no child nodes, its value is the sum of its metadata entries. So, the value of node B is 10+11+12=33, and the value of node D is 99.
+
+However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes. A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on. The value of this node is the sum of the values of the child nodes referenced by the metadata entries. If a referenced child node does not exist, that reference is skipped. A child node can be referenced multiple time and counts each time it is referenced. A metadata entry of 0 does not refer to any child node.
+
+For example, again using the above nodes:
 ```
-Second   Worker 1   Worker 2   Done
-   0        C          .        
-   1        C          .        
-   2        C          .        
-   3        A          F       C
-   4        B          F       CA
-   5        B          F       CA
-   6        D          F       CAB
-   7        D          F       CAB
-   8        D          F       CAB
-   9        D          .       CABF
-  10        E          .       CABFD
-  11        E          .       CABFD
-  12        E          .       CABFD
-  13        E          .       CABFD
-  14        E          .       CABFD
-  15        .          .       CABFDE
-  ```
-Each row represents one second of time. The Second column identifies how many seconds have passed as of the beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle). The Done column shows completed steps.
+Node C has one metadata entry, 2. Because node C has only one child node, 2 references a child node which does not exist, and so the value of node C is 0.
+Node A has three metadata entries: 1, 1, and 2. The 1 references node A's first child node, B, and the 2 references node A's second child node, C. Because node B has a value of 33 and node C has a value of 0, the value of node A is 33+33+0=66.
+So, in this example, the value of the root node is 66.
+```
+What is the value of the root node?
 
-Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
-
-In this example, it would take 15 seconds for two workers to complete these steps.
-
-With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
 
 ### Performance Metrics
 ```
-[B H R T W Y Z C S A E L P U V O I M J K F G Q D X N] 959
-12.354228ms
-
-[B H R T W Y Z C S A E L P U V O I M J K F G Q D X N] 0
-131.262µs
-   10000            292048 ns/op           40788 B/op        420 allocs/op
-PASS
-ok      github.com/atssteve/advent_of_code_2018/day_7/part_2    2.968s
+19990
+1.111872ms
 ```
--- day_8/part_2/main.go
+++ day_8/part_2/main.go
@@ -5,88 +5,55 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
 
-var work []string
-var current string
-var workers []*worker // A slice of workers
+func parseNode(license []int) (int, []int, int) {
+	children := license[:1][0]
+	metadata := license[1:2][0]
+	licenseData := license[2:]
+	count := 0
+	var values []int
 
-var queue [][]string   // Queue from Part 1.
-var donequeue []string // Everything that has completed working in part 2.
-var pt2q []string      // Our queue for part 2.
-
-var cost = make(map[string]int)
-
-// Struct for our worker.
-type worker struct {
-	currentNode string
-	timeLeft    int
-	available   bool
-}
-
-// startWork takes the current worker node and begins the clock.
-func (w *worker) startWork(s string) {
-	w.currentNode = s
-	w.available = false
-	w.timeLeft = cost[s]
-}
-
-// step decrements the time, makes the worker available and adds its node to the done queue.
-func (w *worker) step() {
-	w.timeLeft--
-	if w.timeLeft < 0 {
-		w.timeLeft = 0
-	}
-	if w.timeLeft == 0 && w.available == false {
-		donequeue = append(donequeue, w.currentNode)
-		w.available = true
+	// fmt.Printf("Children: %d\tMetadata: %d\tLicense: %v\n", children, metadata, licenseData[metadata:])
+	for c := 0; c < children; c++ {
+		value := 0
+		result := 0
+		result, licenseData, value = parseNode(licenseData)
+		count += result
+		values = append(values, value)
 	}
 
-}
+	for _, m := range licenseData[:metadata] {
+		count += m
+	}
 
-// Determine if any worker has started working on the current node.
-func hasStarted(s string) bool {
-	for _, w := range workers {
-		if s == w.currentNode {
-			return true
+	value := 0
+	if children == 0 {
+		for _, m := range licenseData[:metadata] {
+			value += m
+		}
+		//fmt.Println(licenseData[:metadata], values)
+		return count, licenseData[metadata:], value
+	}
+	// If metadata is great then zero and in length of metadata. I previous tried to throw away
+	for _, m := range licenseData[:metadata] {
+		if m <= len(values) && m > 0 {
+			value += values[m-1] // Add the previous values
 		}
 	}
-	return false
-}
 
-// To build a dynamic list of work available for part 2. We need to check to see if all of a nodes dependencies are now
-// in the donequeue.
-func areDepsComplete(s string, n map[string][]string) bool {
-	count := 0 // Count needs to be the length of the slice of dependencies.
-	for _, i := range n[s] {
-		for _, q := range donequeue {
-			if s == q { // If this node is already in the donequeue, fail fast.
-				return false
-			}
-			if q == i {
-				count++
-			}
-		}
-	}
-	if len(n[s]) == 0 { // if len is zero, these have already been addressed in the first iteration.
-		return false
-	}
-	if len(n[s]) == count { // All dependencies are in the donequeue.
-		return true
-	}
-
-	return false
+	return count, licenseData[metadata:], value
 }
 
 //Start launches program
 func Start() {
 	t := time.Now()
 
-	nodes := make(map[string][]string)
-	//var order []string
+	var license []int
+
 	// Lets first grab our solutions input
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -97,88 +64,19 @@ func Start() {
 
 	scan := bufio.NewScanner(file)
 
+	// Lets read in our solutions file and store it in a slice of int's (license)
 	for scan.Scan() {
-		s := strings.Split(scan.Text(), " ")
-		nodes[s[7]] = append(nodes[s[7]], s[1])
-		if _, ok := nodes[s[1]]; !ok {
-			nodes[s[1]] = make([]string, 0)
+		b := scan.Text()
+		for _, s := range strings.Split(b, " ") {
+			i, _ := strconv.Atoi(s)
+			license = append(license, i)
 		}
 	}
 
-	// Small refactors to part 1.
-	var complete []string
-	for len(complete) != len(nodes) {
-		for k, v := range nodes {
-			for _, i := range v {
-				if i == string(current) {
-					//nodes[k] = append(nodes[k][:count], nodes[k][count+1:]...)
-				}
-			}
-			if len(nodes[k]) == 0 {
-				work = append(work, k)
-			}
-		}
-		sort.Strings(work)
-		current = work[0]
-		queue = append(queue, work)
-		complete = append(complete, work[0])
-		work = nil
+	_, _, values := parseNode(license)
+	fmt.Println(values)
 
-	}
-
-	// Here is our starting point for part 2. All on the items with no dependencies
-	pt2q := queue[0]
-	fmt.Println(pt2q)
-	fmt.Println(nodes)
-
-	// Lets build the cost of each value now.
-	count := 1
-	for i := 65; i < 65+26; i++ {
-		cost[string(i)] = count + 60
-		count++
-	}
-
-	// Create a slice of workers
-	for i := 0; i < 5; i++ {
-		workers = append(workers, &worker{
-			available: true,
-		})
-	}
-
-	step := 0 // Track our steps
-	// fmt.Printf("Sec\tWorker1\tWorker2\tWorker3\tWorker4\tWorker5\tComplete\n")
-	// fmt.Printf("Sec\tWorker1\tWorker2\tComplete\n")
-	for len(donequeue) < len(nodes) { // While the donequeue does not contain all nodes.
-		for _, i := range pt2q { // range over queue
-			for _, w := range workers { // range over workers
-				if !hasStarted(i) { // If this node is not started on any other node.
-					if w.available { // If this node is available for work
-						w.startWork(i) // Start node working on this worker.
-					}
-				}
-			}
-		}
-		//fmt.Printf("%d\t%s,%d\t%s,%d\t%s,%d\t%s,%d\t%s,%d\t%s\n", step, workers[0].currentNode, workers[0].timeLeft, workers[1].currentNode, workers[1].timeLeft, workers[2].currentNode, workers[2].timeLeft, workers[3].currentNode, workers[3].timeLeft, workers[4].currentNode, workers[4].timeLeft, donequeue)
-		//fmt.Printf("%d\t%s,%d\t%s,%d\t%s\n", step, workers[0].currentNode, workers[0].timeLeft, workers[1].currentNode, workers[1].timeLeft, donequeue)
-		for _, w := range workers {
-			w.step() // Increment the steps on all the workers.
-		}
-
-		// Rebuild the working queue with anything whose dependencies are resolved.
-		pt2q = make([]string, 0)
-		if len(donequeue) != 0 {
-			for k := range nodes {
-				if areDepsComplete(k, nodes) {
-					pt2q = append(pt2q, k)
-				}
-			}
-		}
-		sort.Strings(pt2q)
-		step++
-	}
-	fmt.Println(donequeue, step)
 	fmt.Println(time.Since(t))
-
 }
 
 func main() {
